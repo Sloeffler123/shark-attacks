d0bd9aae5da1737f3c88c150a9d8b824eb2b2fa2
Sloeffler123 <samloeffler123@gmail.com>
2025-04-30 09:05:54 -0500

    all three locations display data

--- lon-lat-converter.py
+++ lon-lat-converter.py
@@ -16,16 +16,10 @@ def get_lat_lon(location, lat, lon):
                 lon.append('None')
                 lat.append('None')
             elif geo:
-                if geo.latitude < -9 and geo.latitude > -50:
-                    print('passed')
-                    print(i)
-                    lon.append(geo.longitude)
-                    lat.append(geo.latitude)
-                else:
-                    print('not in aus')
-                    print(i)
-                    lat.append('None')
-                    lon.append('None')    
+                print('passed')
+                print(i)
+                lon.append(geo.longitude)
+                lat.append(geo.latitude)   
             else:
                 print('didnt pass')
                 print(i)
@@ -41,16 +35,29 @@ def get_lat_lon(location, lat, lon):
 # us_locations_t['Longitude'] = us_longitute
 # us_locations_t.to_csv('us-shark-attack-data', index=False)
 
-aus_locations = pd.read_csv('australia-shark-attack-data')
-aus_locations_t = aus_locations.T
-aus_locations_t.columns = ['Date', 'Location', 'Name', 'Shark species']
+# aus_locations = pd.read_csv('australia-shark-attack-data')
+# aus_locations_t = aus_locations.T
+# aus_locations_t.columns = ['Date', 'Location', 'Name', 'Shark species']
 
-aus_longitude = []
-aus_latitude = []
-get_lat_lon(aus_locations_t, aus_latitude, aus_longitude)
+# aus_longitude = []
+# aus_latitude = []
+# get_lat_lon(aus_locations_t, aus_latitude, aus_longitude)
 
-aus_locations_t['Latitude'] = aus_latitude
-aus_locations_t['Longitude'] = aus_longitude
+# aus_locations_t['Latitude'] = aus_latitude
+# aus_locations_t['Longitude'] = aus_longitude
 
-aus_locations_t.to_csv('aus-backup')
+# aus_locations_t.to_csv('aus-backup')
+
+south_africa_locations = pd.read_csv('southafrica-shark-attack-data')
+south_africa_locations_t = south_africa_locations.T
+south_africa_locations_t.columns = ['Name', 'Date', 'Location', 'Shark species']
+
+south_africa_latitude = []
+south_africa_longitude = []
+get_lat_lon(south_africa_locations_t, south_africa_latitude, south_africa_longitude)
+
+south_africa_locations_t['Latitude'] = south_africa_latitude
+south_africa_locations_t['Longitude'] = south_africa_longitude
+
+south_africa_locations_t.to_csv('south-backup')
 
--- map.py
+++ map.py
@@ -1,42 +1,19 @@
 import pandas as pd
 import plotly.express as px
 
-# us_locations = pd.read_csv('us-shark-attack-data')
+us_locations = pd.read_csv('us-shark-attack-data')
+us_agg_data = us_locations.groupby(['Location', 'Latitude', 'Longitude'], as_index=False).size()
+us_agg_data.rename(columns={'size': 'Count'}, inplace=True)
 
+aus_location = pd.read_csv('australia-shark-attack-data')
+aus_agg_data = aus_location.groupby(['Location', 'Latitude', 'Longitude'], as_index=False).size()
+aus_agg_data.rename(columns={'size': 'Count'}, inplace=True)
 
-# agg_data = us_locations.groupby(['Location', 'Latitude', 'Longitude'], as_index=False).size()
-# agg_data.rename(columns={'size': 'Count'}, inplace=True)
-# fig = px.scatter_geo(agg_data, lat='Latitude', lon='Longitude', hover_name='Location', size='Count')
+south_africa_location = pd.read_csv('southafrica-shark-attack-data')
+south_agg_data = south_africa_location.groupby(['Location', 'Latitude', 'Longitude'], as_index=False).size()
+south_agg_data.rename(columns={'size': 'Count'}, inplace=True)
 
-# fig.show()
+combined_data = pd.concat([us_agg_data, aus_agg_data, south_agg_data], ignore_index=True)
 
-
-
-aus_location = pd.read_csv('aus-backup')
-
-agg_data = aus_location.groupby(['Location', 'Latitude', 'Longitude'], as_index=False).size()
-agg_data.rename(columns={'size': 'Count'}, inplace=True)
-fig = px.scatter_geo(agg_data, lat='Latitude', lon='Longitude', hover_name='Location', size='Count')
-
+fig = px.scatter_geo(combined_data, lat='Latitude', lon='Longitude', hover_name='Location', size='Count')
 fig.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
